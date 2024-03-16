dc92b2dd12784c5b4e24798be79b11d91c192e41
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2024-03-16 16:01:42 +0530

    Update connectToDb.py

--- hello_world/connectToDb.py
+++ hello_world/connectToDb.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://winterprojectiiitm:ZWSAl0qypIwazq8n@communet.mhdgh6b.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("")
 
 db = client["test"]
 
